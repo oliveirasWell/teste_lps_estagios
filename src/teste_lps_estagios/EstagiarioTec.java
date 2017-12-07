a666488328ca73a1db03d9ed5c72a0db7434421e
Wellington Oliveira <well.oliveira.snt@gmail.com>
2017-12-07 13:39:40 -0300

    Removendo lombok

--- src/teste_lps_estagios/EstagiarioTec.java
+++ src/teste_lps_estagios/EstagiarioTec.java
@@ -1,9 +1,7 @@
 package teste_lps_estagios;
 
-import lombok.Builder;
 import negocio.EstagiarioTecnico;
 
-@Builder
 public class EstagiarioTec extends EstagiarioTecnico {
 
     private String codigoEMac;
